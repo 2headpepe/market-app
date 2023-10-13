021b4943c55e89a6e42ce1e26173550051dcb25f
2headpepe <st095279@student.spbu.ru>
2023-10-13 16:11:16 +0300

    Added link to profile

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Header.module.css";
 import Search, { SearchProps } from "antd/es/input/Search";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
@@ -16,24 +17,25 @@ const Header = () => {
         onSearch={onSearch}
         placeholder="Search"
       ></Search>
-
-      <div className={styles.infoWrapper}>
-        <div className={styles.icon}>
-          <div className={styles.money}>0.00$</div>
+      <Link to="/profile">
+        <div className={styles.infoWrapper}>
+          <div className={styles.icon}>
+            <div className={styles.money}>0.00$</div>
+          </div>
+          <div>
+            <div className={styles.name}>Your name</div>
+            <div className={styles.status}>Status</div>
+          </div>
+          <img
+            className={styles.photo}
+            src="https://38s.musify.club/img/68/22744618/58256306.jpg"
+            alt="icon"
+            height={48}
+          />
         </div>
-        <div>
-          <div className={styles.name}>Your name</div>
-          <div className={styles.status}>Status</div>
-        </div>
-        <img
-          className={styles.photo}
-          src="https://38s.musify.club/img/68/22744618/58256306.jpg"
-          alt="icon"
-          height={48}
-        />
-      </div>
+      </Link>
     </nav>
   );
 };
-  
+
 export default Header;
