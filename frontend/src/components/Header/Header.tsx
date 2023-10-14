bcba0bb87839a0a866f12b07d372e491d8201cc5
2headpepe <st095279@student.spbu.ru>
2023-10-14 16:04:12 +0300

    updated header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,38 +2,61 @@ import React from "react";
 import styles from "./Header.module.css";
 import Search, { SearchProps } from "antd/es/input/Search";
 import { Link } from "react-router-dom";
+import MoneyModal from "../Modals/MoneyModal/MoneyModal";
 
 const Header = () => {
   const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
     console.log(info?.source, value);
+
+  const [moneyModal, setMoneyModal] = React.useState(false);
+
+  function handleModal() {
+    setMoneyModal((modal) => !modal);
+  }
+
   return (
     <nav className={styles.NavBar}>
-      <div className={styles.left}>
+      <Link
+        to="/"
+        className={styles.left}
+      >
         <h1>Fake avito</h1>
-      </div>
+      </Link>
 
       <Search
         className={styles.search}
         onSearch={onSearch}
         placeholder="Search"
       ></Search>
-      <Link to="/profile">
-        <div className={styles.infoWrapper}>
-          <div className={styles.icon}>
-            <div className={styles.money}>0.00$</div>
-          </div>
-          <div>
-            <div className={styles.name}>Your name</div>
-            <div className={styles.status}>Status</div>
-          </div>
-          <img
-            className={styles.photo}
-            src="https://38s.musify.club/img/68/22744618/58256306.jpg"
-            alt="icon"
-            height={48}
-          />
+
+      <div className={styles.infoWrapper}>
+        <div
+          className={styles.icon}
+          onClick={handleModal}
+        >
+          <div className="primary2">0.00$</div>
         </div>
-      </Link>
+
+        <Link to="/profile">
+          <div className={styles.info}>
+            <div>
+              <div className="primary">Your name</div>
+              <div className="secondary">Status</div>
+            </div>
+            <img
+              className={styles.photo}
+              src="https://38s.musify.club/img/68/22744618/58256306.jpg"
+              alt="icon"
+              height={48}
+            />
+          </div>
+        </Link>
+      </div>
+
+      <MoneyModal
+        modal={moneyModal}
+        setModal={setMoneyModal}
+      />
     </nav>
   );
 };
