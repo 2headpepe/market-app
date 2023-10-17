7cd9aff5f06310f46810c6407a247615be7615db
2headpepe <st095279@student.spbu.ru>
2023-10-17 16:26:21 +0300

    Added component for uploading photos

---
+++ frontend/src/components/Modals/PhotoModal/PhotoModal.tsx
@@ -0,0 +1,13 @@
+import Modal from "../Modal/Modal";
+import UploadPhoto from "../../UploadPhoto/UploadPhoto";
+import ModalProps from "../ModalTypes";
+
+const PhotoModal = ({ modal, setModal }: ModalProps) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default PhotoModal;
---
+++ frontend/src/components/UploadPhoto/UploadPhoto.tsx
@@ -0,0 +1,30 @@
+import { PlusOutlined, UploadOutlined } from "@ant-design/icons";
+import { Button, Modal, Upload, UploadFile } from "antd";
+import { RcFile, UploadProps } from "antd/es/upload";
+import React from "react";
+
+import "./UploadPhoto.css";
+
+const UploadPhoto = () => {
+  const [fileList, setFileList] = React.useState<UploadFile[]>([]);
+
+  return (
+    <>
+      <Upload
+        action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+        listType="picture"
+        defaultFileList={[...fileList]}
+        className="upload-list-inline"
+      >
+        <Button
+          icon={<UploadOutlined />}
+          className="ant-upload-list-item"
+        >
+          Upload
+        </Button>
+      </Upload>
+    </>
+  );
+};
+
+export default UploadPhoto;
