56c9a4e340712de2c655927ad389aee0ccdad751
Ivan <gorgeouswintered@gmail.com>
2023-11-12 15:27:26 +0300

    Added token to headers of request

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,3 +1,25 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosHeaders } from "axios";
+
+import Endpoints from './endpoints'
+import { store } from "../store";
+import { getAccessToken } from "../store/auth/actionCreators";
 
 export const axiosInstance = axios.create()
+
+const urlsSkipAuth = [Endpoints.AUTH.LOGIN, Endpoints.AUTH.REFRESH, Endpoints.AUTH.LOGOUT]
+
+axiosInstance.interceptors.request.use(async (config) => {
+    if (config.url && urlsSkipAuth.includes(config.url)) {
+        return config
+    }
+
+    const accessToken = await store.dispatch(getAccessToken())
+
+    if (accessToken) {
+        const authorization = `Bearer ${accessToken}`
+
+        config.headers['Authorization'] = authorization;
+    }
+
+    return config
+})
