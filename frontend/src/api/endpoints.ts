d9d57026e23b8b01ee6c0dedeb32839e0cb80b8f
Ivan <gorgeouswintered@gmail.com>
2023-11-12 15:28:03 +0300

    Updated api

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,19 +1,42 @@
 const Endpoints = {
   AUTH: {
-    LOGIN: "http://89.108.71.29:8003/api/auth/authenticate",
+    LOGIN: "/api/auth/authenticate",
+    REGISTER: "/api/auth/register",
     REFRESH: "/refresh",
     LOGOUT: "/logout",
-    PROFILE: "/api/v1/users/profile",
+    PROFILE: "/api/users/profile",
   },
   LISTINGS:{
-    MY_LISTINGS:"/api/v1/listings/myListings",
+    MY_LISTINGS:"/api/listings/myListings",
+    CREATE_LISTING:"/api/listings",
+    GET_LISTING:"/api/listings/",
+    BUY_LISTING:"/api/listings/",
+    DELETE_LISTING:"/api/listings/",
+    USER_LISTINGS:"/api/listings/userListings/",
+    SEARCH_LISTINGS:"/api/listings/search",
   },
   IMAGES:{
-    LISTING_IMAGES:"/api/v1/images/{listingId}"
+    LISTING_IMAGES:"/api/images"
   },
   ORDERS:{
-    SELLS:"/api/v1/orders/sells",
-    BUYS:"/api/v1/orders/buys"
+    SELLS:"/api/orders/sells",
+    BUYS:"/api/orders/buys"
+  },
+  ADMIN:{
+    GET_USERS:"/api/admin/users",
+    CREATE_CATEGORY:"/api/admin/categories",
+    DELETE_CATEGORY:"/api/admin/categories/",
+    DELETE_USER:"/api/admin/users/",
+    GET_WITHDRAWS:"/api/admin/withdraws",
+    DELETE_WITHDRAW:"/api/admin/withdraws/",
+    GET_DEPOSITS:"/api/admin/deposits",
+    DELETE_DEPOSITS:"/api/admin/deposits/",
+  },
+  DEPOSIT:"/api/deposits",
+  WITHDRAW:"/api/withdraws",
+  CATEGORY:{
+    GET_CATEGORIES:"/api/categories",
+    GET_CATEGORY:"/api/categories/",
   }
 };
 
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,12 +2,20 @@ import * as auth from "./auth";
 import * as images from "./images"
 import * as listings from "./listings";
 import * as orders from "./orders"
+import * as admin from "./admin"
+import * as withdraw from "./withdraw"
+import * as deposit from "./deposit"
+import * as category from "./category"
 
 const api = {
   auth,
   images,
   listings,
-  orders
+  orders,
+  admin,
+  withdraw,
+  deposit,
+  category
 };
 
 export default api;
